50ba14be596c3629782554bdd06269509f24a554
Vera Rojman <vrojman@protonmail.com>
2021-12-24 18:47:39 +0100

    Lint

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
-  mount Decidim::Core::Engine => '/'
+  mount Decidim::Core::Engine => "/"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
